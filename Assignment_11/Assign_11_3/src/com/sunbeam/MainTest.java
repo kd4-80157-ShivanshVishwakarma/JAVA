256b8b1f6e82b58c160a3804f3011c835351f804
RetenX <shivanshvishwakarma59@gmail.com>
2023-10-19 22:17:11 +0530

    AssignQ3

---
+++ Assignment_11/Assign_11_3/src/com/sunbeam/MainTest.java
@@ -0,0 +1,27 @@
+package com.sunbeam;
+
+import java.util.*;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Set<StoreBook> st=new TreeSet<>();
+		
+		st.add(new StoreBook("4545",400,"B",5));
+		st.add(new StoreBook("2575",500,"C",2));
+		st.add(new StoreBook("4545",600,"K",3));
+		st.add(new StoreBook("4545",200,"I",4));
+		st.add(new StoreBook("1545",400,"P",5));
+		st.add(new StoreBook("6545",300,"O",7));
+		
+		Iterator<StoreBook> itr=st.iterator();
+		while(itr.hasNext()) {
+			StoreBook s=itr.next();
+			System.out.println(s);
+		}
+	}
+
+}
+
+
